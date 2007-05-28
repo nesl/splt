57ec2af3929b6a745fac5067113cc22be13ee202
kimyh <kimyh@79999026-cc2e-0410-813b-c2c382b76d16>
2007-05-28 22:13:55 +0000

    retrieve

--- pySensorbase/example_retrieve.py
+++ pySensorbase/example_retrieve.py
@@ -3,5 +3,7 @@ from Retrieve import retrieve
 # input values are (table_name, fields, condition, data_from, number of data(max = 10000))
 # return values are csv data
 
-print retrieve.retrieve('PowerMeasure',"AID,TimeStamp",1,10000,10000)
+
+# the following means, from the "PowerMeasure" table, get AID, TimeStamp, Watt, Voltage and A data from 0 to 10000
+print retrieve.retrieve('PowerMeasure',"AID,TimeStamp,W,V,A",1,0,10000)
 
