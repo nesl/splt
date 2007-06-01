de3d11ca6a6f0f74eaa42736761eb0d0eaecb9e0
zcharbi <zcharbi@79999026-cc2e-0410-813b-c2c382b76d16>
2007-06-01 01:55:46 +0000

    custom kernels for now for rfid and pm since they are on seperate motes

---
+++ pm/sos/rfid_app/module_test_app.c
@@ -0,0 +1,21 @@
+
+#include <sos.h>
+/**
+ * Must also include the header file that defines the 
+ * blink_get_header()
+ */
+
+mod_header_ptr blink_get_header();
+mod_header_ptr uart_mod_get_header();
+mod_header_ptr rfid_get_header();
+
+/**
+ * application start
+ * This function is called once at the end od SOS initialization
+ */
+void sos_start(void)
+{
+ker_register_module(blink_get_header());
+ker_register_module(uart_mod_get_header());
+ker_register_module(rfid_get_header());
+}
