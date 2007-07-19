07e4d02a550ddd9f9d94770686465d1d499e3015
kimyh <kimyh@79999026-cc2e-0410-813b-c2c382b76d16>
2007-07-19 20:46:54 +0000

    photo added

---
+++ backend/photo.py
@@ -0,0 +1,45 @@
+import sys
+import time
+import os
+import Queue
+import thread
+from pysos import pysos
+from Slog import spltSlog
+
+
+# Message Queue
+RFIDqueue = Queue.Queue(200)
+PMqueue = Queue.Queue(200)
+
+
+
+
+class photoModule:
+	def __init__(self):
+		try:
+			self.srv = pysos.sossrv()
+		except:
+			print "Please run sos_server first"
+		try:
+			thread.start_new_thread(self.getSOSmsg,())
+		except thread.error:
+			pass
+
+
+	def getSOSmsg(self):
+		while True:
+			try:
+				msg = self.srv.listen()
+				pkt = msg[0]['data']
+				photoMsg = pysos.unpack('>BH',pkt)
+				if photoMsg[0]==1:
+					print photoMsg
+			except:
+				pass
+		
+
+
+if(__name__ == "__main__"):
+	dummy = photoModule()
+	while True:
+		pass
