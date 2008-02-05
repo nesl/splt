6aaaad49317f26ae706682aec5a9bc00eb5249bb
kimyh <kimyh@79999026-cc2e-0410-813b-c2c382b76d16>
2008-02-05 22:33:21 +0000

    moved prototype implementation

--- sosroomba/pcmote/pcmote.h
+++
@@ -1,8 +0,0 @@
-#ifndef _BLINK_H_
-#define _BLINK_H_
-
-#ifndef _MODULE_
-#endif
-
-#endif
-
--- sosroomba/roombamote/roombamote.h
+++
@@ -1,9 +0,0 @@
-#ifndef _BLINK_H_
-#define _BLINK_H_
-
-#ifndef _MODULE_
-int8_t uart_roomba_init();
-#endif
-
-#endif
-
